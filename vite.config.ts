b1045212184378afc96a5aa2d36a6a3e1019c5ff
Steidl, Tim (SE-A/34) <tim.steidl@volkswagen.de>
2024-10-21 21:27:41 +0200

    Initial Push

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import path from "path"
+import viteReact from "@vitejs/plugin-react"
+import {defineConfig} from "vite"
+
+export default defineConfig({
+    plugins: [viteReact()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+})
